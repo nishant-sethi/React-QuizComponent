39859cdd43a4427ce3a405fdeaccbd32459de762
nishantsethi <nsethi610@gmail.com>
2020-01-12 17:45:16 +0530

    Displaying Error

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,11 +6,24 @@ class QuizQuestion extends Component {
         super(props);
         this.clickHandler = this.handleClick.bind(this);
         this.state = {
+            incorrectAnswer: false
         }
     }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+            this.setState(state => {
+                return {
+                    incorrectAnswer: false
+                };
+            });
+        }
+        else {
+            this.setState(state => {
+                return {
+                    incorrectAnswer: true
+                };
+            })
         }
     }
     render() {
@@ -21,6 +34,8 @@ class QuizQuestion extends Component {
                         {this.props.quiz_question.instruction_text}
                     </p>
                 </section>
+                {this.state.incorrectAnswer ?
+                    <div className="error">Sorry, that's not right </div> : null}
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer, index) => {
